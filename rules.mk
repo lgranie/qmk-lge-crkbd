9bb4cce6dec23416fc7a2be3fc8f38960f0c7768
Laurent Granié <laurent.granie@inetum.world>
2021-08-31 15:44:11 +0200

    update led config

--- rules.mk
+++ rules.mk
@@ -1,4 +1,7 @@
-OLED_DRIVER_ENABLE = yes
-RGBLIGHT_ENABLE    = yes       # Enable WS2812 RGB underlight.
-RGB_MATRIX_ENABLE  = no
-NKRO_ENABLE        = yes
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+RGBLIGHT_ENABLE   = yes       # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = no
