e83d553ef44fe0050c129efb84e085d1fb92aad8
Laurent Granié <laurent.granie@gmail.com>
2024-03-12 09:12:45 +0100

    Update Copyright year

--- keymap.c
+++ keymap.c
@@ -1,7 +1,7 @@
 /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
-Copyright 2022 Laurent Granié
+Copyright 2024 Laurent Granié
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
